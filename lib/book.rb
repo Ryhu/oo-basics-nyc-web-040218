95452df311dd7620f9bd31ed36934dcd3a5364ad
Ryan Hu <ryanhuuu@gmail.com>
2018-04-04 15:55:34 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,12 @@
 class Book 
-  attr_accessor :title, :author_name, :page_count, :genre
+  attr_accessor :title, :author, :page_count, :genre
   def initialize(input)
     @title = input
   end
   
   
   def turn_page
-    
+    puts "Flipping the page...wow, you read fast!"
   end
   
 end
